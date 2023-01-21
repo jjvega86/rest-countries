6e58e81a84bcdab2f629a22f4e72fa4faf5fbc4d
JJ <jj.vega86@gmail.com>
2023-01-21 06:34:20 -0500

    👌 IMPROVE: Moved fonts to layout component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,20 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Header from "@/components/Header";
+import { Nunito_Sans } from "@next/font/google";
+
+const nunito_sans = Nunito_Sans({
+  weight: ["300", "600", "800"],
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Header />
-      <Component {...pageProps} />
+      <main className={nunito_sans.className}>
+        <Component {...pageProps} />
+      </main>
     </>
   );
 }
--- src/pages/detail.tsx
+++ src/pages/detail.tsx
@@ -1,10 +1,4 @@
 import Head from "next/head";
-import { Nunito_Sans } from "@next/font/google";
-
-const nunito_sans = Nunito_Sans({
-  weight: ["300", "600", "800"],
-  subsets: ["latin"],
-});
 
 export default function Detail() {
   return (
@@ -15,9 +9,9 @@ export default function Detail() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={nunito_sans.className}>
+      <section>
         <h1 className="mt-5">DETAIL</h1>
-      </main>
+      </section>
     </>
   );
 }
