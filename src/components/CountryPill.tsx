d61ea38ff00f9f3d6192a5a86efe2e6861ac10d9
JJ <jj.vega86@gmail.com>
2023-01-21 18:25:10 -0500

    👌 IMPROVE: Added loading and error states to CountryPill

--- src/components/CountryPill.tsx
+++ src/components/CountryPill.tsx
@@ -1,18 +1,23 @@
 import { fetcher } from "@/util/helpers";
 import useSWR from "swr";
+import Link from "next/link";
 
 interface CountryPillProps {
   countryCode: string;
 }
 
 const CountryPill = ({ countryCode }: CountryPillProps) => {
-  const { data, error } = useSWR(
+  const { data, error, isLoading } = useSWR(
     `https://restcountries.com/v3.1/alpha/${countryCode}`,
     fetcher
   );
-  console.log(data);
+  if (isLoading) return <p>Loading...</p>;
   if (error) return <p>No border countries</p>;
-  return <p>{data[0]?.name.common}</p>;
+  return (
+    <Link href={`/${data[0].name.common}`}>
+      <p>{data && data[0].name.common}</p>
+    </Link>
+  );
 };
 
 export default CountryPill;
